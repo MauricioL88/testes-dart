82fa2fd03303103c630ef5edb36821ca7ec37cfb
MauricioL88 <lisboa.sse@gmail.com>
2022-08-06 23:37:38 -0300

    Aula Constante

---
+++ bin/Fundamentos/constantes.dart
@@ -0,0 +1,30 @@
+void main() {
+  var lista = ['Ana', 'Lia', 'Gui'];
+  print(lista);
+  print('');
+  // Usando 'var' a lista se torna variável
+  lista.add('Rebecca');
+  print(lista);
+  print('');
+
+  /*
+    Usando o 'final' não é possível criar uma novo tipo de lista,
+    mas é possível modificá-la, como: adicionar um novo item,
+    remover, entre outros...
+  */
+  final frutas = ['Banana', 'Maçã', 'Uva'];
+  print(frutas);
+  print('');
+  // frutas = ['Carro', 'Moto', 'Bicicleta'];
+  frutas.add('Kiwi');
+  print(frutas);
+
+  /*
+    * Usando o 'const' não é possível alterar o tipo da lista nem o
+    * seu conteúdo.
+  */
+
+  final alunos = const ['Maurício', 'Evelin'];
+
+  print(alunos);
+}
